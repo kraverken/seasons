9b23709b659e8af1c756bf25baf53205e64cf7ee
Aryan <aryanverma170372@gmail.com>
2022-10-11 00:33:05 +0530

    feat: getting users physical location

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+  return <div>Hello</div>;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
